4628621b02cebd65743ff1b774262d2cd3830ff8
Mohammad Abdulhakim <duxwork0@gmail.com>
2026-04-03 13:56:57 +0200

    feat: integrate helmet middleware for enhanced application security

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,17 @@ import { PostgresErrorFilter } from './common/filters/postgres-error.filter';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const trustProxy = process.env.TRUST_PROXY ?? 'loopback';
   app.set('trust proxy', trustProxy);
+
+  app.use(helmet({
+    contentSecurityPolicy: false,
+  }));
 
   const defaultCorsOrigins = ['http://localhost:3001', 'http://localhost:3000'];
   const configuredCorsOrigins = process.env.CORS_ORIGINS
